42f97a737c533ff684e6a841ff036f2cffad557c
Emir Zecic <emir506@hotmail.com>
2022-08-26 07:20:08 +0200

    AlarmConfigData.cs created for LowPressureThreshold and HighPressureThreshold values

---
+++ src/TirePressureMonitoringSystem/AlarmConfigData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    internal static class AlarmConfigData
+    {
+        public static double LowPressureThreshold = 17;
+        public static double HighPressureThreshold = 21;
+
+    }
+}
