9a3d6f7ec44d88c73bcfe3e101489fc497da24a3
Emir Zecic <emir506@hotmail.com>
2022-08-26 07:08:42 +0200

    created simple NUnit test before refactoring

---
+++ src/UnitTest/AlarmTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    [TestFixture]
+    internal class NUnitTest
+    {
+        [TestCase]
+        public void CheckIfAlarmDefaultValue_IsFalse()
+        {
+            bool defaultAlarmStatus = false;
+            Alarm alarm = new Alarm();
+            Assert.AreEqual(defaultAlarmStatus, alarm.AlarmOn);
+        }
+
+    }
+}
